776ae3d82a0c2d7bb4b2cfbe9ff762db502ec680
Eyad-Helmy <meyad8375@gmail.com>
2025-05-19 00:17:47 +0300

    initial set up

---
+++ eventhub-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-green-600">
+        Tailwind is working!
+      </h1>
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Test Button
+      </button>
+    </div>
+  )
+}
+export default App
